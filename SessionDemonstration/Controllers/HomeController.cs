d7c0295783c7c60eba985362c9ff14d19a0234be
Utpal Maiti <Utpalmaiti12@gmail.com>
2020-07-05 23:22:17 +0530

    Add project files.

---
+++ SessionDemonstration/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SessionDemonstration.Controllers
+{
+    //<!-- Disabled at Controller level -->
+    //[SessionState(System.Web.SessionState.SessionStateBehavior.Disabled)]
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            //Session.Remove()
+            //Session.RemoveAll();
+            //Session.Clear();
+
+            //Session.Abandon();
+            Session["uName"] = "Pragim";
+            Session["pwd"] = "Pragim@123";
+            ViewBag.Demo = "DemoText";
+
+            return View();
+        }
+
+        public ActionResult EditProfile()
+        {
+            //Session.Abandon();
+            return View();
+        }
+
+        public ActionResult ChangePassword()
+        {
+            return View();
+        }
+    }
+}
